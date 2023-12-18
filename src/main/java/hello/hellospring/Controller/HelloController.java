95e7a7410089cb7f6746b998d6525ff1122bd54f
cogns <cogns8571@naver.com>
2023-12-18 22:33:21 +0900

    Mvc and template engine

--- src/main/java/hello/hellospring/Controller/HelloController.java
+++ src/main/java/hello/hellospring/Controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //MVC의 Controller
 @Controller
@@ -14,5 +15,19 @@ public class HelloController {
         // 매핑된 후 data를 찾아서 Value인 hello를 넘겨준다.
         model.addAttribute("data", "hello");
         return "hello"; // templates/hello.html을 매핑한다.
+    }
+
+    /*
+    * 웹에서 파라미터 받기
+    * @RequestParam
+    * */
+    @GetMapping("hello-mvc") // localhost:8080/hello-mvc
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        /*
+        * model에 담으면 view에서 렌더링할 때 사용한다.
+        * 파라미터로 넘어온 name을 넘긴다.
+        */
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
