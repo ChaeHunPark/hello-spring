3cece2ab4c229dc71a042a97f85958d8209d62d0
cogns <cogns8571@naver.com>
2024-01-05 04:15:09 +0900

    Web MVC development

---
+++ src/main/java/hello/hellospring/Controller/MemberForm.java
@@ -0,0 +1,15 @@
+package hello.hellospring.Controller;
+
+
+/* 폼에서 들어오는 값*/
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
